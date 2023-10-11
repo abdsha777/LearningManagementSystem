9ff902d94966e45d0d6bc00ff661f0370f16a2f6
abdsha777 <abdullashaikh3211@gmail.com>
2023-10-11 19:47:24 +0530

    Update server.js

--- db/server.js
+++ db/server.js
@@ -7,6 +7,6 @@ const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use('/', router)
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 7000, () => {
   console.log('JSON Server is running')
 })
