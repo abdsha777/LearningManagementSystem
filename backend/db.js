a766115fbd1e528318bb12ac84c228b472f94958
abdsha777 <abdullashaikh3211@gmail.com>
2023-10-11 18:57:29 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 async function connectToMongo() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/lms');
-    console.log("connected")
+
+    const username = process.env.mongoUser || null;
+    const password = process.env.mongo || null;
+    if(username==null || password ==null){
+        await mongoose.connect('mongodb://127.0.0.1:27017/lms');
+        console.log("connected without env")
+    }else{
+        await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.bztvjye.mongodb.net/?retryWrites=true&w=majority`);
+        console.log("connected")
+    }
 }
 
 module.exports = connectToMongo
