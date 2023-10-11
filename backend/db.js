ecce958665df1a573e302b8cc56b62ef28f5f519
abdsha777 <abdullashaikh3211@gmail.com>
2023-10-11 19:09:31 +0530

    added env for hosting

--- backend/db.js
+++ backend/db.js
@@ -8,7 +8,7 @@ async function connectToMongo() {
         await mongoose.connect('mongodb://127.0.0.1:27017/lms');
         console.log("connected without env")
     }else{
-        await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.bztvjye.mongodb.net/?retryWrites=true&w=majority`);
+        await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.bztvjye.mongodb.net/lms?retryWrites=true&w=majority`);
         console.log("connected")
     }
 }
