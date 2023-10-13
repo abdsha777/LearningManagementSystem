475243ffe701d9a2abe297179051b6d4009dcba9
AyeshaChhagala <ayesha.chhagla@gmail.com>
2023-10-13 16:06:57 +0530

    student list endpoints

--- frontend/src/pages/admin/AdminAddStudent.jsx
+++ frontend/src/pages/admin/AdminAddStudent.jsx
@@ -1,49 +1,116 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 function AdminAddStudent() {
-    return (
+  const endpoint = "http://localhost:7000";
+  const [student, setStudent] = useState({});
+  const { id } = useParams();
+  const nav = useNavigate();
+  const getStudentList = () => {
+    fetch(`${endpoint}/Admin_Teacher_student/` + id)
+      .then((response) => response.json())
+      .then((data) => setStudent(data))
+      .catch((error) => console.log);
+  };
+  const handleChange = (e) => {
+    setStudent({
+      ...student,
+      [e.target.name]: e.target.value,
+    });
+  };
+  function updateStudent() {
+    const init = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(student),
+    }
+    fetch(`${endpoint}/Admin_Teacher_student/` + id, init)
+    .then((response) => response.json())
+    .then((data) => nav("/adminStudentlist"))
+    // .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+  }
+  function addStudent() {
+    const init = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(student),
+    }
+    fetch(`${endpoint}/Admin_Teacher_student/`, init)
+    .then((response) => response.json())
+    .then((data) => nav("/adminStudentlist"))
+    // .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    if(id)getStudentList();
+  }, []);
+
+  return (
+    <div>
+      <div className="main">
         <div>
-            <div className="main">
-                <div >
-                    <h2>Add Student</h2>
-                </div>
+          <h2>{id?"Update":"Add"} Student</h2>
+        </div>
 
-                <div className="add_form">
-                    <div className="input-box">
-                        <label htmlFor="cname">Student Name</label>
-                        <input type="text" id="cname" placeholder="Name" />
-                    </div>
-                    <div className="input-box">
-                        <p>Faculty</p>
-                        <select name="" id="">
-                            <option value="">Arts</option>
-                            <option value="">Scince</option>
-                            <option value="">commerce</option>
-                        </select>
+        <div className="add_form">
+          <div className="input-box">
+            <label htmlFor="cname">Student Name</label>
+            <input
+              type="text"
+              id="cname"
+              placeholder="Name"
+              name="name"
+              value={student.name || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-box">
+            <p>Faculty</p>
+            <select name="faculty" id="faculty" value={student.faculty || ""} onChange={handleChange}>
+              <option value="---">---</option>
+              <option value="arts">Arts</option>
+              <option value="science">Scince</option>
+              <option value="commerce">commerce</option>
+            </select>
+          </div>
+          <div className="input-box">
+            <label htmlFor="year">Year</label>
+            <input
+              type="text"
+              id="year"
+              name="year"
+              placeholder="Year"
+              value={student.year || ""}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
 
-                    </div>
-                    <div className="input-box">
-                        <label htmlFor="cname">Year</label>
-                        <input type="text" id="cname" placeholder="Year" />
-                    </div>
+        <div className="module_btn">
+          <p> Module:</p>
+          <button className="btn btn-border-blue">+ Add Module</button>
+        </div>
+      </div>
 
-                </div>
+      <div className="cancel_save">
+        <button className="btn btn-border cancel-btn">Cancel</button>
 
-                <div className="module_btn">
-                    <p> Module:</p>
-                    <button className="btn btn-border-blue">+ Add Module</button>
-                </div>
-
-            </div>
-
-            <div className="cancel_save">
-                <button className="btn btn-border cancel-btn">Cancel</button>
-
-                <button className="btn btn-filled">Save</button>
-
-            </div>
-        </div >
-    )
+        {
+            id?(
+                <button className="btn btn-filled" onClick={updateStudent}>Update</button>
+            ):(
+                <button className="btn btn-filled" onClick={addStudent}>Update</button>
+            )
+        }
+      </div>
+    </div>
+  );
 }
 
-export default AdminAddStudent
+export default AdminAddStudent;
