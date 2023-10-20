c03ded739966234db97afcfdf279aa47508fe155
abdsha777 <abdullashaikh3211@gmail.com>
2023-10-20 15:32:32 +0530

    studentlist new

---
+++ frontend/src/components/Lists/StudentList.jsx
@@ -0,0 +1,261 @@
+import React from 'react'
+import './StudentList.css'
+
+function StudentList() {
+    return (
+        <div className='studentListNew'>
+            <div className="listHeading">
+                <h1>STUDENT LIST</h1>
+                <button>+</button>
+            </div>
+            <div className="searchSection">
+                <input type="text" name="query" placeholder='Search by name...' />
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="23" viewBox="0 0 22 23" fill="none">
+                        <path d="M20.2889 22.5L12.5889 14.625C11.9778 15.125 11.275 15.5208 10.4806 15.8125C9.68611 16.1042 8.84074 16.25 7.94444 16.25C5.72407 16.25 3.84511 15.4633 2.30756 13.89C0.77 12.3167 0.000814815 10.395 0 8.125C0 5.85417 0.769185 3.9325 2.30756 2.36C3.84593 0.7875 5.72489 0.000833333 7.94444 0C10.1648 0 12.0438 0.786667 13.5813 2.36C15.1189 3.93333 15.8881 5.855 15.8889 8.125C15.8889 9.04167 15.7463 9.90625 15.4611 10.7188C15.1759 11.5312 14.7889 12.25 14.3 12.875L22 20.75L20.2889 22.5ZM7.94444 13.75C9.47222 13.75 10.771 13.2029 11.8409 12.1088C12.9107 11.0146 13.4453 9.68667 13.4444 8.125C13.4444 6.5625 12.9095 5.23417 11.8397 4.14C10.7698 3.04583 9.47141 2.49917 7.94444 2.5C6.41667 2.5 5.11785 3.04708 4.048 4.14125C2.97815 5.23542 2.44363 6.56333 2.44444 8.125C2.44444 9.6875 2.97937 11.0158 4.04922 12.11C5.11907 13.2042 6.41748 13.7508 7.94444 13.75Z" fill="#555555" />
+                    </svg>
+                    Search
+                </button>
+            </div>
+            <section className="listSection">
+
+                <div className="filterBox">
+                    <div className="filterHead">
+                        <h2>Filter</h2>
+                    </div>
+                    <div className="filterBody">
+                        <div className="filter">
+                            <div className="filterInfo">
+                                <h4>Class</h4>
+                                <button>Reset</button>
+                            </div>
+                            <select className='filterDropDown' name="class">
+                                <option value="FYBCA" selected>FYBCA</option>
+                                <option value="SYBCA" >SYBCA</option>
+                                <option value="TYBCA">TYBCA</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="filterBottom">
+                        <button>Reset</button>
+                        <button className='apply'>Apply</button>
+                    </div>
+                </div>
+
+                <div className="listBox">
+                    <table className='listTable'>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Class</th>
+                                <th>Enrolled</th>
+                                <th>Completed</th>
+                                <th>Status</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr>
+                                <td><input type="checkbox" /></td>
+                                <td>
+                                    <div className="studentDetails">
+                                        <div className="img"></div>
+                                        <div className="studentInfo">
+                                            <small>14771</small>
+                                            <h3>Sohail Momin</h3>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>TYBCA</td>
+                                <td>10</td>
+                                <td>8</td>
+                                <td><div className="status"></div></td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
+                                        <path d="M6.79175 23.9373L13.5951 23.9135L28.4445 8.80851C29.0272 8.21001 29.3479 7.41518 29.3479 6.56968C29.3479 5.72418 29.0272 4.92934 28.4445 4.33084L25.9994 1.81967C24.8339 0.622675 22.8004 0.629008 21.6442 1.81492L6.79175 16.9231V23.9373ZM23.8195 4.05851L26.2692 6.56493L23.8071 9.06976L21.362 6.56018L23.8195 4.05851ZM9.87508 18.2436L19.1713 8.78634L21.6164 11.2975L12.3217 20.7516L9.87508 20.7595V18.2436Z" fill="#1E1E1E" />
+                                        <path d="M3.70833 30.25H25.2917C26.9921 30.25 28.375 28.8298 28.375 27.0833V13.359L25.2917 16.5257V27.0833H8.57692C8.53683 27.0833 8.49521 27.0992 8.45513 27.0992C8.40425 27.0992 8.35337 27.0849 8.30096 27.0833H3.70833V4.91667H14.2641L17.3475 1.75H3.70833C2.00787 1.75 0.625 3.17025 0.625 4.91667V27.0833C0.625 28.8298 2.00787 30.25 3.70833 30.25Z" fill="#1E1E1E" />
+                                    </svg>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td><input type="checkbox" /></td>
+                                <td>
+                                    <div className="studentDetails">
+                                        <div className="img"></div>
+                                        <div className="studentInfo">
+                                            <small>87621</small>
+                                            <h3>Huzefa Baig</h3>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>TYBCA</td>
+                                <td>10</td>
+                                <td>8</td>
+                                <td><div className="status"></div></td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
+                                        <path d="M6.79175 23.9373L13.5951 23.9135L28.4445 8.80851C29.0272 8.21001 29.3479 7.41518 29.3479 6.56968C29.3479 5.72418 29.0272 4.92934 28.4445 4.33084L25.9994 1.81967C24.8339 0.622675 22.8004 0.629008 21.6442 1.81492L6.79175 16.9231V23.9373ZM23.8195 4.05851L26.2692 6.56493L23.8071 9.06976L21.362 6.56018L23.8195 4.05851ZM9.87508 18.2436L19.1713 8.78634L21.6164 11.2975L12.3217 20.7516L9.87508 20.7595V18.2436Z" fill="#1E1E1E" />
+                                        <path d="M3.70833 30.25H25.2917C26.9921 30.25 28.375 28.8298 28.375 27.0833V13.359L25.2917 16.5257V27.0833H8.57692C8.53683 27.0833 8.49521 27.0992 8.45513 27.0992C8.40425 27.0992 8.35337 27.0849 8.30096 27.0833H3.70833V4.91667H14.2641L17.3475 1.75H3.70833C2.00787 1.75 0.625 3.17025 0.625 4.91667V27.0833C0.625 28.8298 2.00787 30.25 3.70833 30.25Z" fill="#1E1E1E" />
+                                    </svg>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" /></td>
+                                <td>
+                                    <div className="studentDetails">
+                                        <div className="img"></div>
+                                        <div className="studentInfo">
+                                            <small>14771</small>
+                                            <h3>Sohail Momin</h3>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>TYBCA</td>
+                                <td>10</td>
+                                <td>8</td>
+                                <td><div className="status"></div></td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
+                                        <path d="M6.79175 23.9373L13.5951 23.9135L28.4445 8.80851C29.0272 8.21001 29.3479 7.41518 29.3479 6.56968C29.3479 5.72418 29.0272 4.92934 28.4445 4.33084L25.9994 1.81967C24.8339 0.622675 22.8004 0.629008 21.6442 1.81492L6.79175 16.9231V23.9373ZM23.8195 4.05851L26.2692 6.56493L23.8071 9.06976L21.362 6.56018L23.8195 4.05851ZM9.87508 18.2436L19.1713 8.78634L21.6164 11.2975L12.3217 20.7516L9.87508 20.7595V18.2436Z" fill="#1E1E1E" />
+                                        <path d="M3.70833 30.25H25.2917C26.9921 30.25 28.375 28.8298 28.375 27.0833V13.359L25.2917 16.5257V27.0833H8.57692C8.53683 27.0833 8.49521 27.0992 8.45513 27.0992C8.40425 27.0992 8.35337 27.0849 8.30096 27.0833H3.70833V4.91667H14.2641L17.3475 1.75H3.70833C2.00787 1.75 0.625 3.17025 0.625 4.91667V27.0833C0.625 28.8298 2.00787 30.25 3.70833 30.25Z" fill="#1E1E1E" />
+                                    </svg>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td><input type="checkbox" /></td>
+                                <td>
+                                    <div className="studentDetails">
+                                        <div className="img"></div>
+                                        <div className="studentInfo">
+                                            <small>87621</small>
+                                            <h3>Huzefa Baig</h3>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>TYBCA</td>
+                                <td>10</td>
+                                <td>8</td>
+                                <td><div className="status"></div></td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
+                                        <path d="M6.79175 23.9373L13.5951 23.9135L28.4445 8.80851C29.0272 8.21001 29.3479 7.41518 29.3479 6.56968C29.3479 5.72418 29.0272 4.92934 28.4445 4.33084L25.9994 1.81967C24.8339 0.622675 22.8004 0.629008 21.6442 1.81492L6.79175 16.9231V23.9373ZM23.8195 4.05851L26.2692 6.56493L23.8071 9.06976L21.362 6.56018L23.8195 4.05851ZM9.87508 18.2436L19.1713 8.78634L21.6164 11.2975L12.3217 20.7516L9.87508 20.7595V18.2436Z" fill="#1E1E1E" />
+                                        <path d="M3.70833 30.25H25.2917C26.9921 30.25 28.375 28.8298 28.375 27.0833V13.359L25.2917 16.5257V27.0833H8.57692C8.53683 27.0833 8.49521 27.0992 8.45513 27.0992C8.40425 27.0992 8.35337 27.0849 8.30096 27.0833H3.70833V4.91667H14.2641L17.3475 1.75H3.70833C2.00787 1.75 0.625 3.17025 0.625 4.91667V27.0833C0.625 28.8298 2.00787 30.25 3.70833 30.25Z" fill="#1E1E1E" />
+                                    </svg>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" /></td>
+                                <td>
+                                    <div className="studentDetails">
+                                        <div className="img"></div>
+                                        <div className="studentInfo">
+                                            <small>14771</small>
+                                            <h3>Sohail Momin</h3>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>TYBCA</td>
+                                <td>10</td>
+                                <td>8</td>
+                                <td><div className="status"></div></td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
+                                        <path d="M6.79175 23.9373L13.5951 23.9135L28.4445 8.80851C29.0272 8.21001 29.3479 7.41518 29.3479 6.56968C29.3479 5.72418 29.0272 4.92934 28.4445 4.33084L25.9994 1.81967C24.8339 0.622675 22.8004 0.629008 21.6442 1.81492L6.79175 16.9231V23.9373ZM23.8195 4.05851L26.2692 6.56493L23.8071 9.06976L21.362 6.56018L23.8195 4.05851ZM9.87508 18.2436L19.1713 8.78634L21.6164 11.2975L12.3217 20.7516L9.87508 20.7595V18.2436Z" fill="#1E1E1E" />
+                                        <path d="M3.70833 30.25H25.2917C26.9921 30.25 28.375 28.8298 28.375 27.0833V13.359L25.2917 16.5257V27.0833H8.57692C8.53683 27.0833 8.49521 27.0992 8.45513 27.0992C8.40425 27.0992 8.35337 27.0849 8.30096 27.0833H3.70833V4.91667H14.2641L17.3475 1.75H3.70833C2.00787 1.75 0.625 3.17025 0.625 4.91667V27.0833C0.625 28.8298 2.00787 30.25 3.70833 30.25Z" fill="#1E1E1E" />
+                                    </svg>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td><input type="checkbox" /></td>
+                                <td>
+                                    <div className="studentDetails">
+                                        <div className="img"></div>
+                                        <div className="studentInfo">
+                                            <small>87621</small>
+                                            <h3>Huzefa Baig</h3>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>TYBCA</td>
+                                <td>10</td>
+                                <td>8</td>
+                                <td><div className="status"></div></td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
+                                        <path d="M6.79175 23.9373L13.5951 23.9135L28.4445 8.80851C29.0272 8.21001 29.3479 7.41518 29.3479 6.56968C29.3479 5.72418 29.0272 4.92934 28.4445 4.33084L25.9994 1.81967C24.8339 0.622675 22.8004 0.629008 21.6442 1.81492L6.79175 16.9231V23.9373ZM23.8195 4.05851L26.2692 6.56493L23.8071 9.06976L21.362 6.56018L23.8195 4.05851ZM9.87508 18.2436L19.1713 8.78634L21.6164 11.2975L12.3217 20.7516L9.87508 20.7595V18.2436Z" fill="#1E1E1E" />
+                                        <path d="M3.70833 30.25H25.2917C26.9921 30.25 28.375 28.8298 28.375 27.0833V13.359L25.2917 16.5257V27.0833H8.57692C8.53683 27.0833 8.49521 27.0992 8.45513 27.0992C8.40425 27.0992 8.35337 27.0849 8.30096 27.0833H3.70833V4.91667H14.2641L17.3475 1.75H3.70833C2.00787 1.75 0.625 3.17025 0.625 4.91667V27.0833C0.625 28.8298 2.00787 30.25 3.70833 30.25Z" fill="#1E1E1E" />
+                                    </svg>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><input type="checkbox" /></td>
+                                <td>
+                                    <div className="studentDetails">
+                                        <div className="img"></div>
+                                        <div className="studentInfo">
+                                            <small>14771</small>
+                                            <h3>Sohail Momin</h3>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>TYBCA</td>
+                                <td>10</td>
+                                <td>8</td>
+                                <td><div className="status"></div></td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
+                                        <path d="M6.79175 23.9373L13.5951 23.9135L28.4445 8.80851C29.0272 8.21001 29.3479 7.41518 29.3479 6.56968C29.3479 5.72418 29.0272 4.92934 28.4445 4.33084L25.9994 1.81967C24.8339 0.622675 22.8004 0.629008 21.6442 1.81492L6.79175 16.9231V23.9373ZM23.8195 4.05851L26.2692 6.56493L23.8071 9.06976L21.362 6.56018L23.8195 4.05851ZM9.87508 18.2436L19.1713 8.78634L21.6164 11.2975L12.3217 20.7516L9.87508 20.7595V18.2436Z" fill="#1E1E1E" />
+                                        <path d="M3.70833 30.25H25.2917C26.9921 30.25 28.375 28.8298 28.375 27.0833V13.359L25.2917 16.5257V27.0833H8.57692C8.53683 27.0833 8.49521 27.0992 8.45513 27.0992C8.40425 27.0992 8.35337 27.0849 8.30096 27.0833H3.70833V4.91667H14.2641L17.3475 1.75H3.70833C2.00787 1.75 0.625 3.17025 0.625 4.91667V27.0833C0.625 28.8298 2.00787 30.25 3.70833 30.25Z" fill="#1E1E1E" />
+                                    </svg>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td><input type="checkbox" /></td>
+                                <td>
+                                    <div className="studentDetails">
+                                        <div className="img"></div>
+                                        <div className="studentInfo">
+                                            <small>87621</small>
+                                            <h3>Huzefa Baig</h3>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>TYBCA</td>
+                                <td>10</td>
+                                <td>8</td>
+                                <td><div className="status"></div></td>
+                                <td>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" viewBox="0 0 30 31" fill="none">
+                                        <path d="M6.79175 23.9373L13.5951 23.9135L28.4445 8.80851C29.0272 8.21001 29.3479 7.41518 29.3479 6.56968C29.3479 5.72418 29.0272 4.92934 28.4445 4.33084L25.9994 1.81967C24.8339 0.622675 22.8004 0.629008 21.6442 1.81492L6.79175 16.9231V23.9373ZM23.8195 4.05851L26.2692 6.56493L23.8071 9.06976L21.362 6.56018L23.8195 4.05851ZM9.87508 18.2436L19.1713 8.78634L21.6164 11.2975L12.3217 20.7516L9.87508 20.7595V18.2436Z" fill="#1E1E1E" />
+                                        <path d="M3.70833 30.25H25.2917C26.9921 30.25 28.375 28.8298 28.375 27.0833V13.359L25.2917 16.5257V27.0833H8.57692C8.53683 27.0833 8.49521 27.0992 8.45513 27.0992C8.40425 27.0992 8.35337 27.0849 8.30096 27.0833H3.70833V4.91667H14.2641L17.3475 1.75H3.70833C2.00787 1.75 0.625 3.17025 0.625 4.91667V27.0833C0.625 28.8298 2.00787 30.25 3.70833 30.25Z" fill="#1E1E1E" />
+                                    </svg>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+
+                    <div className="listBulkActions">
+                        <div>
+                            <input type="checkbox" name="selectAll" id='selectAll' />
+                            <label htmlFor="selectAll">Select ALL</label>
+                        </div>
+
+                        <button className='activate'>Activate</button>
+                        <button className='deactivate'>Deactivate</button>
+                        <button className='bulkApply'>Apply</button>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    )
+}
+
+export default StudentList;
--- frontend/src/pages/admin/AdminStudentList.jsx
+++ frontend/src/pages/admin/AdminStudentList.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext';
 import Popup from "reactjs-popup";
 import StudentFilter from '../../components/popups/StudentFilter';
+import StudentList from '../../components/Lists/StudentList';
 
 function AdminStudentList() {
   // const dbJson = "https://jsonserver-6gyk.onrender.com";
@@ -21,9 +22,9 @@ function AdminStudentList() {
       .catch(error => console.log(error))
     setLoading(false)
   }
-  useEffect(() => {
-    getStudentList();
-  }, [])
+  // useEffect(() => {
+  //   getStudentList();
+  // }, [])
 
   const StudentSetStatus = () => (
     <Popup trigger={<button className="btn btn-filled big">Set Status</button>} modal className="set-status">
@@ -69,78 +70,79 @@ function AdminStudentList() {
   );
 
   return (
-    <div className="main_module">
-      <h1 className="list-heading">Students List</h1>
+    // <div className="main_module">
+    //   <h1 className="list-heading">Students List</h1>
 
-      <div className="module2">
-        <button className="btn btn-border-blue"><Link to={"add/"}>+ Add Students</Link></button>
-        <button className="btn btn-border-blue">Bulk Upload</button>
-      </div>
+    //   <div className="module2">
+    //     <button className="btn btn-border-blue"><Link to={"add/"}>+ Add Students</Link></button>
+    //     <button className="btn btn-border-blue">Bulk Upload</button>
+    //   </div>
 
-      <div className="sub_heading">
-        <input type="text" placeholder="Search Students" />
+    //   <div className="sub_heading">
+    //     <input type="text" placeholder="Search Students" />
 
-        <StudentFilter />
-      </div>
+    //     <StudentFilter />
+    //   </div>
 
 
-      <div className="module2">
-        {/* <button className="btn btn-filled big">Set Status</button> */}
-        <StudentSetStatus />
-        <div id="popup-root" />
-      </div>
+    //   <div className="module2">
+    //     {/* <button className="btn btn-filled big">Set Status</button> */}
+    //     <StudentSetStatus />
+    //     <div id="popup-root" />
+    //   </div>
 
-      <div className="module5">
-        { loading?(
-          <h1>Loading...</h1>
-          ):(
-          <table>
-            <thead>
-              <tr>
-                {/* <th>Student Id</th> */}
-                <th>Student Name</th>
-                <th>Faculty</th>
-                <th>Year</th>
-                <th>Total No of <br /> Enrolled Courses</th>
-                <th>Ongoing Course</th>
-                <th>Completed Course</th>
-                <th>Update</th>
-                <th>View</th>
-                <th className="status">Active</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                student.map((record, key) => {
-                  return (
-                    <tr key={key}>
-                      {/* <td>{record.id}</td> */}
-                      <td>{record.name}</td>
-                      <td>{record.faculty}</td>
-                      <td>{record.year}</td>
-                      <td>{record.enroll}</td>
-                      <td>{record.ongoing}</td>
-                      <td>{record.completed}</td>
-                      <td>
-                        <Link to={'update/' + record.id} ><button>Update</button></Link>
-                      </td>
-                      <td>
-                        <button>View</button>
-                      </td>
-                      <td>
-                        <button className="status">{record.status}</button>
-                      </td>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
+    //   <div className="module5">
+    //     { loading?(
+    //       <h1>Loading...</h1>
+    //       ):(
+    //       <table>
+    //         <thead>
+    //           <tr>
+    //             {/* <th>Student Id</th> */}
+    //             <th>Student Name</th>
+    //             <th>Faculty</th>
+    //             <th>Year</th>
+    //             <th>Total No of <br /> Enrolled Courses</th>
+    //             <th>Ongoing Course</th>
+    //             <th>Completed Course</th>
+    //             <th>Update</th>
+    //             <th>View</th>
+    //             <th className="status">Active</th>
+    //           </tr>
+    //         </thead>
+    //         <tbody>
+    //           {
+    //             student.map((record, key) => {
+    //               return (
+    //                 <tr key={key}>
+    //                   {/* <td>{record.id}</td> */}
+    //                   <td>{record.name}</td>
+    //                   <td>{record.faculty}</td>
+    //                   <td>{record.year}</td>
+    //                   <td>{record.enroll}</td>
+    //                   <td>{record.ongoing}</td>
+    //                   <td>{record.completed}</td>
+    //                   <td>
+    //                     <Link to={'update/' + record.id} ><button>Update</button></Link>
+    //                   </td>
+    //                   <td>
+    //                     <button>View</button>
+    //                   </td>
+    //                   <td>
+    //                     <button className="status">{record.status}</button>
+    //                   </td>
+    //                 </tr>
+    //               )
+    //             })
+    //           }
+    //         </tbody>
 
-          </table>
-          )
-        } 
-      </div>
-    </div>
+    //       </table>
+    //       )
+    //     } 
+    //   </div>
+    // </div>
+    <StudentList />
   )
 }
 
