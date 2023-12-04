4155aea9a70f11f8fbe550529ee68c4a51d9a59a
abdsha777 <abdullashaikh3211@gmail.com>
2023-12-04 23:15:22 +0530

    Update TeacherMenu.jsx

--- frontend/src/components/menu/TeacherMenu.jsx
+++ frontend/src/components/menu/TeacherMenu.jsx
@@ -42,7 +42,7 @@ function TeacherMenu({ menu, showMenu }) {
                 </svg>
                 My Courses
             </NavLink>
-            <NavLink to={'/'} className="sidebar-links" onClick={showMenu}>
+            <NavLink to={'addCourse/'} className="sidebar-links" onClick={showMenu}>
                 <svg className="add-svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
 
